0e6a179f273f5875be1b4713f0ea1c6bd98072d3
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2025-07-09 18:59:43 +0530

    seting up layouts

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,31 @@
-import { Text } from 'react-native'
+import { Dimensions, Image, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, Text, View } from 'react-native'
 import React from 'react'
 import { Slot } from 'expo-router'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '@/constants'
+import Custominput from '@/components/Custominput'
+import Custombutton from '@/components/Custombutton'
+
 const _layout = () => {
     return (
-        <SafeAreaView>
-            <Text>Auth layout</Text>
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
+            <ScrollView className='bg-white h-full' keyboardShouldPersistTaps="handled">
+                <View className='w-full relative' style={{ height: Dimensions.get("screen").height / 2.25 }}>
+                    <ImageBackground source={images.loginGraphic} className='size-full rounded-b-lg' resizeMode='stretch' />
+                    <Image source={images.logo} className='self-center size-48 absolute -bottom-16 z-10' />
+
+                </View>
+                <Custominput 
+                    placeholder='Enter your email'
+                    value={''}
+                    onChangeText={(text) => {}}
+                    label='Email'
+                    keyboardType='email-address'
+                />
+                <Custombutton/>
+
+            </ScrollView>
             <Slot />
-        </SafeAreaView>
+        </KeyboardAvoidingView>
     )
 }
 
---
+++ components/Custombutton.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Custombutton = () => {
+  return (
+    <View>
+      <Text>Custombutton</Text>
+    </View>
+  )
+}
+
+export default Custombutton
